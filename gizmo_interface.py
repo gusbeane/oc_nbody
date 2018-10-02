a3d270749fb7cdee4ab0f65fe40f295c7695cf49
Gus Beane <abeane@sas.upenn.edu>
2018-10-02 15:34:46 -0400

    Exclude stars further than Rmax based on recalculated galaxy center

--- gizmo_interface.py
+++ gizmo_interface.py
@@ -177,12 +177,18 @@ class gizmo_interface(object):
         self.time_in_Myr = self._time_in_Myr_()
 
     def _clean_Rmag_(self, snap):
+        # recalculate center position
+        old_center_position = snap.center_position
+        gizmo.io.Read.assign_center(snap)
         for key in snap.keys():
-            rmag = np.linalg.norm(snap[key].prop('host.distance.principal'),
-                                  axis=1)
+            rmag = snap[key].prop('host.distance.total')
             rmag_keys = np.where(rmag < self.Rmax)[0]
             for dict_key in snap[key].keys():
                 snap[key][dict_key] = snap[key][dict_key][rmag_keys]
+        # reassign center position
+        snap.center_position = old_center_position
+        for key in snap.keys():
+            snap[key].center_position = old_center_position
         return snap
 
     def _assign_self_center_(self, part):
