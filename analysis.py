Fix units in cluster animator

analysis.py
@@ -239,7 +239,7 @@ class cluster_animator(object):
 
         self.fig, self.ax = plt.subplots(1)
         self.ax.axis('equal')
-        self.scat = self.ax.scatter(first_x, first_y, s=first_mass)
+        self.scat = self.ax.scatter(first_x, first_y, s=first_mass, c='k')
         if acc_map:
             if interface is None or options is None:
                 raise Exception('Please provide interface and options file')
@@ -295,8 +295,8 @@ class cluster_animator(object):
             sys.exit(1)
 
     def _animate_(self, frame, scat, im=None):
-        this_x_data = self.snapshots[frame]['position'][:, self._xaxis_key_]/1000.0
-        this_y_data = self.snapshots[frame]['position'][:, self._yaxis_key_]/1000.0
+        this_x_data = self.snapshots[frame]['position'][:, self._xaxis_key_]
+        this_y_data = self.snapshots[frame]['position'][:, self._yaxis_key_]
         this_mass = self.snapshots[frame]['mass']
         if self.mass_max is not None:
             keys = np.where(this_mass < self.mass_max)[0]