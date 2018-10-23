965423498c57813f51ea4088accedb88e0087307
Angus Beane <abeane-intern@flatironinstitute.org>
2018-10-22 21:39:00 -0400

    Fix colorbar, actually v2

--- analysis.py
+++ analysis.py
@@ -352,7 +352,7 @@ class cluster_animator(object):
             self.im = self.ax.imshow(this_hm, extent=self.extent, origin='lower',
                             vmin=self.cmin, vmax=self.cmax, cmap=self.cmap,
                             animated=True)
-            colorbar(label='acc '+self.acc + '[km/s/Myr]', cax=self.ax)
+            plt.colorbar(label='acc '+self.acc + '[km/s/Myr]', cax=self.ax)
 
         self.ax.set_xlim(xmin, xmax)
         self.ax.set_ylim(ymin, ymax)
